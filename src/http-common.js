1805e8dec9a0601b6fd1f261ee0655c49000d1d6
EstebanRoHe <esteban_rojashe@hotmail.com>
2023-07-24 23:07:54 -0600

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -6,7 +6,7 @@ const api = axios.create({
  baseURL: 'https://back-end-rent-a-car-in-spring-boot.onrender.com',
   headers: {
     'Content-Type': 'application/json'
-    ,'Access-Control-Allow-Origin': '*' 
+    //,'Access-Control-Allow-Origin': '*' 
   }
 });
 
