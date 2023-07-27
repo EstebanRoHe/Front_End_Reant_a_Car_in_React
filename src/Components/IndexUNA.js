24eee1a9940075bd85f8c07d94ea9cbadc53ce8c
EstebanRoHe <esteban_rojashe@hotmail.com>
2023-07-26 23:45:32 -0600

    update
    update

--- src/Components/IndexUNA.js
+++ src/Components/IndexUNA.js
@@ -17,11 +17,11 @@ const IndexUNA = () => {
                 <div >
                     <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" >
                         <div className="carousel-inner tamano">
-                            <div className="carousel-item active">
-                                <img src={logo} className="d-block w-100" alt="logo" />
+                        <div className="carousel-item active ">
+                                <img src={logo} className="d-block w-100" alt="..." />
                             </div>
-
-                            <div className="carousel-item">
+                          
+                            <div className="carousel-item ">
                                 <img src={prado} className= "d-block w-100 imagen-carousel" alt="..." />
                             </div>
 
